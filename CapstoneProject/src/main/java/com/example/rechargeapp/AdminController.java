6dc16b3732e7626cc19e70b997d667adac85809b
Rohan-negi <rohan.negi@freecharge.com>
2021-10-18 10:07:37 +0530

    added few more skeleton classes

--- CapstoneProject/src/main/java/com/example/rechargeapp/AdminController.java
+++ CapstoneProject/src/main/java/com/example/rechargeapp/AdminController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.rechargeapp;
 
 public class AdminController {
 
---
+++ CapstoneProject/src/main/java/com/example/rechargeapp/BankAccountService.java
@@ -0,0 +1,5 @@
+package com.example.rechargeapp;
+
+public class BankAccountService {
+
+}
