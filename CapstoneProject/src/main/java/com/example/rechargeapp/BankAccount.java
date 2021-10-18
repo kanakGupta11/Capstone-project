22f637698a33edec2a9f0638e71aa8af92ace9bc
Rohan-negi <rohan.negi@freecharge.com>
2021-10-18 11:09:21 +0530

    added account, payment repo interface and model class
    added account, payment repo interface and model class for table creation

--- CapstoneProject/src/main/java/com/example/rechargeapp/BankAccount.java
+++ CapstoneProject/src/main/java/com/example/rechargeapp/BankAccount.java
@@ -1,5 +1,28 @@
 package com.example.rechargeapp;
 
-public class BankAccount {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class BankAccount {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer accountId;
+	private int accountNumber;
+	private String bankName;
+	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getBankName() {
+		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
 }
--- CapstoneProject/src/main/java/com/example/rechargeapp/Payment.java
+++ CapstoneProject/src/main/java/com/example/rechargeapp/Payment.java
@@ -1,5 +1,43 @@
 package com.example.rechargeapp;
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Payment {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer paymentId;
+	private Integer customerId;
+	private String customerName;
+	private Date dateOfPayment;
+	private Integer planId;
+	
+	public Integer getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public Date getDateOfPayment() {
+		return dateOfPayment;
+	}
+	public void setDateOfPayment(Date dateOfPayment) {
+		this.dateOfPayment = dateOfPayment;
+	}
+	public Integer getPlanId() {
+		return planId;
+	}
+	public void setPlanId(Integer planId) {
+		this.planId = planId;
+	}
 }
