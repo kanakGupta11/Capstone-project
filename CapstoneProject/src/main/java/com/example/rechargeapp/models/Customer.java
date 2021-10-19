911d9a98b443795b44e055e03e455d136333a045
SaurabhArya07 <91590346+SaurabhArya07@users.noreply.github.com>
2021-10-20 00:09:44 +0530

    Security service added
    Security service added to customer class,
    signup and login operations

--- CapstoneProject/src/main/java/com/example/rechargeapp/models/Customer.java
+++ CapstoneProject/src/main/java/com/example/rechargeapp/models/Customer.java
@@ -1,45 +1,111 @@
-
 package com.example.rechargeapp.models;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sun.istack.NotNull;
 
 @Entity
+@Table( name = "customers",
+        uniqueConstraints = {
+        		@UniqueConstraint(columnNames="email")
+        })
 public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer customerId;
 	
-	@Column(nullable=false)
+	@NotNull
 	private String firstName;
 	
 	@NotNull
 	private String lastName;
 	
-	@Column(nullable=false, length=64)
+	@NotBlank
+	@Size(max=30)
+	private String username;
+	
+	@NotNull
+	@Size(max=120)
 	private String password;
 	
-	@Column(nullable=false)
+	@NotNull
 	private double mobileNumber;
 	
-	@Column(nullable=false, unique=true, length=45)
+	
+	@Size(max=50)
+	@Email
 	private String email;
 	
-	@Column(nullable=false)
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dateOfBirth;
 	
 	private String gender;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date createdDate;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date updatedDate;
 	
+	@ManyToMany(fetch=FetchType.LAZY)
+	@JoinTable(name="user_roles",
+	           joinColumns = @JoinColumn(name="user_id"),
+	           inverseJoinColumns = @JoinColumn(name="role_id"))
+	private Set<Role> roles = new HashSet<>();
 	
+	public Customer() {
+	}
+	
+	public Customer(String username,String email, String firstName, String lastName, String password, double mobileNumber, String gender,
+			        Date dateOfBirth, Date createdDate, Date updatedDate) {
+		this.username = username;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.mobileNumber = mobileNumber;
+		this.gender = gender;
+		this.dateOfBirth = dateOfBirth;
+		this.createdDate = createdDate;
+		this.updatedDate = updatedDate;
+		
+	}
+	
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
 	public Integer getCustomerId() {
 		return customerId;
 	}
@@ -100,6 +166,8 @@ public class Customer {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	
 	
 	
 	
--- CapstoneProject/src/main/java/com/example/rechargeapp/service/CustomerService.java
+++ CapstoneProject/src/main/java/com/example/rechargeapp/service/CustomerService.java
@@ -2,7 +2,9 @@ package com.example.rechargeapp.service;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.rechargeapp.models.Customer;
@@ -13,14 +15,18 @@ public class CustomerService {
 	@Autowired
 	CustomerRepository customerRepository;
 	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
 	public void save(Customer customer) {
 		customerRepository.save(customer);
 		System.out.println("Signed Up successfully!!");
 	}
-
+	
 	public List<Customer> findAllCustomers() {
 		return customerRepository.findAll();
 	}
+	
 	
 	
 	
